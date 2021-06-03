b873c82494dc70a30d50e9721a099aa9995e1dcd
Orijma <remymarquentin@gmail.com>
2021-06-03 16:50:31 +0200

    add quest5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -18,6 +18,15 @@ Class ProgramController extends AbstractController
             'website' => 'Wild Séries',
             ]);
         }
+
+   /**
+     * @Route("{id}", requirements={"id"="\d+"}, methods={"GET"}, name="show")
+     */
+
+     public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', ['id' => $id]);
+    }
         
 
 }
